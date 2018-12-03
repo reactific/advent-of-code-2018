13677986c773a7d0738e994cdbad42b818858963
Reid Spencer <reid@reactific.com>
2018-12-03 12:21:02 -0500

    Complete first gold start for Day 2

--- src/main/scala/com/reactific/aoc2018/Day02.scala
+++ src/main/scala/com/reactific/aoc2018/Day02.scala
@@ -1,6 +1,5 @@
 package com.reactific.aoc2018
 
-import scala.collection.mutable
 import scala.io.Source
 
 object Day02 {
@@ -10,6 +9,17 @@ object Day02 {
     // Read data from resource file
     val input = Source.fromResource("Day02.txt").getLines().filterNot(_.isEmpty).toVector
 
+    case class Tally(twos: Int, threes: Int) {
+      def printHash(): Unit = println(twos * threes)
+    }
+
+    input.foldLeft(Tally(0,0)) {
+      case (tally: Tally, string: String) =>
+        val groups = string.groupBy(identity)
+        val duplicates = if (groups.exists { case (_: Char, s: String) => s.length == 2}) 1 else 0
+        val triplicates =  if (groups.exists { case (_: Char, s: String) => s.length == 3}) 1 else 0
+        Tally(tally.twos + duplicates, tally.threes + triplicates)
+    }.printHash()
 
   }
 }
