3f59794a0fa4abf11f1d743f327f75acc0d8eff5
Reid Spencer <reid@reactific.com>
2018-12-03 11:56:53 -0500

    Framework For Day2

---
+++ src/main/scala/com/reactific/aoc2018/Day02.scala
@@ -0,0 +1,15 @@
+package com.reactific.aoc2018
+
+import scala.collection.mutable
+import scala.io.Source
+
+object Day02 {
+
+  def main(args: Array[String]): Unit = {
+
+    // Read data from resource file
+    val input = Source.fromResource("Day02.txt").getLines().filterNot(_.isEmpty).toVector
+
+
+  }
+}
